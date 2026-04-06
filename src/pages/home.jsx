a621268d840b692f4f69a0400a2ccf3fc385071b
catalina <catalinaruedaf@hotmail.com>
2026-04-06 16:26:33 +1000

    chcking the bmi claculator

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,13 @@
 
 import Main from "../components/Main.jsx";
 import HeroSection from "../components/HeroSection.jsx";
-// import HeroBmiCalculator from "../components/HeroBmiCalculation.jsx";
+import HeroBmiCalculator from "../components/HeroBmiCalculation.jsx";
 
 export default function Home () {
     return (
         <>
            <HeroSection/>
+           <BmiCalculation/>
            {/* <HeroBmiCalculator/> */}
            <Main/>
            
