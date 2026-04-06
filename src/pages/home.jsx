8866b777a9560a62958d3b9c0ee98a57d8062f7d
catalina <catalinaruedaf@hotmail.com>
2026-04-06 16:40:09 +1000

    some changes

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,7 @@
 
 import Main from "../components/Main.jsx";
 import HeroSection from "../components/HeroSection.jsx";
-import HeroBmiCalculator from "../components/HeroBmiCalculation.jsx";
+import BmiCalculation from "../components/BmiCalculation.jsx";
 
 export default function Home () {
     return (
